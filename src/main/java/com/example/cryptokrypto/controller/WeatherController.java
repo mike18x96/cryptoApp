ad3668402ddc48930b1b5c7c4d58badc849f6d68
mike18x96@gmail.com <lD3hq%x2WfYvZuQ5rkynKw*>
2021-08-22 15:56:55 +0200

    [change] - getWeatherById endpoin with ResponseEntity

--- src/main/java/com/example/cryptokrypto/controller/WeatherController.java
+++ src/main/java/com/example/cryptokrypto/controller/WeatherController.java
@@ -3,7 +3,10 @@ package com.example.cryptokrypto.controller;
 import com.example.cryptokrypto.dto.WeatherDto;
 import com.example.cryptokrypto.service.WeatherService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,17 @@ public class WeatherController {
     public List<WeatherDto> getAll(){
         log.info("getAll");
         return weatherService.getAllWeather();
+    }
+
+    @GetMapping("/weathers/{id}")
+    public ResponseEntity<WeatherDto>  getWeatherById(@PathVariable("id") Long id) {
+        log.info("getWeatherById: [{}]", id);
+        var result = weatherService.findWeatherById(id);
+
+        if (result.isPresent()) {
+            return new ResponseEntity<>(result.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
