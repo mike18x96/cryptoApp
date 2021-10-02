0c3b897f8db16550a2fd2b51d4ea8303c6c383ee
mike18x96@gmail.com <lD3hq%x2WfYvZuQ5rkynKw*>
2021-10-02 13:22:49 +0200

    [change] - components first taste

---
+++ angular-ui/src/app/services/weather.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor() {}
+
+  myName(): string{
+    return "artur"
+  }
+
+}
